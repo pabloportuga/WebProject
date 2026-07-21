ea5e103a3c88ff62c12352dfe6c68ffa213469bd
Pablo Portugal <pabloportuga0@gmail.com>
2026-07-20 22:07:13 -0300

    feat:test POST no server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Servidor funcionando!");
 });
-module.exports = app;
 
-app.use(express.json());
 app.post("/users", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      message: "Todos os campos são obrigatórios."
+    });
+  }
+  console.log(req.body);
 
+  res.status(201).json({
+    message: "Usuário recebido com sucesso!"
+  });
 });
+module.exports = app;
