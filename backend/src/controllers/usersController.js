284cd8eae1913f09f96f4857f98147bbe1343e56
Pablo Portugal <pabloportuga0@gmail.com>
2026-07-28 15:14:26 -0300

    feat: erros e teste do POST na tabela users

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -1,13 +1,16 @@
 const pool = require("../database/connection")
 async function createUser(req, res) {
   try {
+    console.log(req.body)
     const { username, email, password } = req.body;
     const valores = [username, email, password]
-    console.log(req.body)
-    await pool.query('INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3)', valores)
-
-    res.status(201).json({ message: "Usuário criado com sucesso!" });
-
+    // teste para valores nulos ou em branco
+    if (!username || !email || !password || username.trim() === '' || email.trim() === '' || password.trim() === '') {
+      return res.status(400).json({ message: "Os valores não podem ser nulos ou em branco!" });
+    } else {
+      await pool.query('INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3)', valores)
+      return res.status(201).json({ message: "Usuário criado com sucesso!" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Um erro aconteceu!" });
