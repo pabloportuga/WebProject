04f8aa11d8d504241ef88fb20ba2eb80d87193c4
Pablo Portugal <pabloportuga0@gmail.com>
2026-07-30 21:00:03 -0300

    feat: rotas de auth/login adicionadas

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 
 const usersRoutes = require("./routes/users")
-
+const authRoutes = require("./routes/auth");
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando!");
 });
 
-app.use("/users", usersRoutes)
+app.use("/users", usersRoutes);
+app.use("/login", authRoutes);
 
 module.exports = app;
