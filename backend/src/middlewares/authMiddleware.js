9de078404e2e520517e4737b7aafa5b9a3809b33
Pablo Portugal <pabloportuga0@gmail.com>
2026-08-02 21:18:40 -0300

    feat: função validarToken implementada

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,9 +1,40 @@
 const jwt = require("jsonwebtoken")
-// recebe token
-// valida
-// token valido?
-// sim
-// req.user =
-// proxima rota
-// não
-// 401
+require('dotenv').config();
+
+async function validarToken(req, res, next) {
+  try {
+    const authHeader = req.header("authorization");
+    if (!authHeader) {
+      return res.status(401).json({
+        message: "Acesso negado!"
+      });
+    }
+    const partes = authHeader.split(" ");
+
+    if (partes.length !== 2) {
+      return res.status(401).json({
+        message: "Header com mais ou menos de duas partes!"
+        //mudar em produção para Authorization header inválido
+      });
+    }
+    const [bearer, token] = partes
+    if (bearer !== "Bearer") {
+      return res.status(401).json({
+        message: "Bearer não encontrado!"
+        //mudar em produção para Authorization header inválido
+      });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+    return next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({
+      message: "Token inválido ou expirado!"
+    });
+  }
+}
+module.exports = {
+  validarToken
+};
