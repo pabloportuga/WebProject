85f952f229dde44d25775d4dd08e9d3a72858932
Pablo Portugal <pabloportuga0@gmail.com>
2026-08-02 21:20:08 -0300

    feat: adicionado a rota /me

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-router.post('/', authController.login);
+const authMiddleware = require("../middlewares/authMiddleware")
 
+
+router.post('/login', authController.login);
+router.get("/me", authMiddleware.validarToken, authController.me)
 module.exports = router;
